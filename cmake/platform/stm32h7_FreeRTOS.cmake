e3cbeaa3daafa5f63443cb06549a306baf2580ef
Howard-GitHub <howard.l.general@gmail.com>
2026-04-09 01:25:44 -0700

    Replace old repository name with new

--- cmake/platform/stm32h7_FreeRTOS.cmake
+++ cmake/platform/stm32h7_FreeRTOS.cmake
@@ -5,9 +5,9 @@
 if(NOT DEFINED ARDUINO_FQBN)
     message(FATAL_ERROR "Must defined arduino FQBN")
 elseif(CMAKE_SYSTEM_PROCESSOR STREQUAL "arm")
-    set(ARDUINO_TYPES_DIR "${FPRIME_PROJECT_ROOT}/lib/fprime-featherm4-freertos/cmake/platform/arm/Platform") 
+    set(ARDUINO_TYPES_DIR "${FPRIME_PROJECT_ROOT}/lib/fprime-nucleo_h723zg-freertos/cmake/platform/arm/Platform") 
 else()
-    set(ARDUINO_TYPES_DIR "${FPRIME_PROJECT_ROOT}/lib/fprime-featherm4-freertos/cmake/platform/basic/Platform")
+    set(ARDUINO_TYPES_DIR "${FPRIME_PROJECT_ROOT}/lib/fprime-nucleo_h723zg-freertos/cmake/platform/basic/Platform")
 endif()
 
 set(CMAKE_EXECUTABLE_SUFFIX "${FPRIME_ARDUINO_EXECUTABLE_SUFFIX}" CACHE INTERNAL "" FORCE)
@@ -17,7 +17,7 @@ set(CMAKE_EXECUTABLE_SUFFIX "${FPRIME_ARDUINO_EXECUTABLE_SUFFIX}" CACHE INTERNAL
 set(FPRIME_PLATFORM "ArduinoFw")
 
 include_directories("${CMAKE_CURRENT_LIST_DIR}")
-include_directories("${FPRIME_PROJECT_ROOT}/lib/fprime-featherm4-freertos/fprime-arduino")
-include_directories("${FPRIME_PROJECT_ROOT}/lib/fprime-featherm4-freertos/fprime-freertos")
+include_directories("${FPRIME_PROJECT_ROOT}/lib/fprime-nucleo_h723zg-freertos/fprime-arduino")
+include_directories("${FPRIME_PROJECT_ROOT}/lib/fprime-nucleo_h723zg-freertos/fprime-freertos")
 include_directories("${CMAKE_BINARY_DIR}/fprime-arduino")
 add_fprime_subdirectory("${ARDUINO_TYPES_DIR}")
