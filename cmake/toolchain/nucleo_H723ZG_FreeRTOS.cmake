8e6ff896002785c8fa4e79901820b26f236d4457
Howard-GitHub <howard.l.general@gmail.com>
2026-06-05 17:05:13 -0700

    Modify toolchain to include generic toolchain in fprime-arduino

--- cmake/toolchain/nucleo_H723ZG_FreeRTOS.cmake
+++ cmake/toolchain/nucleo_H723ZG_FreeRTOS.cmake
@@ -3,4 +3,18 @@
 # Alias for the current STM32H723ZG FreeRTOS toolchain configuration.
 ####
 
-include("${CMAKE_CURRENT_LIST_DIR}/stm32h7_FreeRTOS.cmake")
+# include("${CMAKE_CURRENT_LIST_DIR}/stm32h7_FreeRTOS.cmake")
+
+set(FPRIME_PLATFORM "stm32h7_FreeRTOS")
+set(FPRIME_USE_BAREMETAL_SCHEDULER OFF)  
+include(${CMAKE_CURRENT_LIST_DIR}/../../fprime-arduino/cmake/toolchain/nucleo_H723ZG.cmake)
+
+set(ARDUINO_BUILD_PROPERTIES
+	"compiler.c.extra_flags=-DINCLUDE_xSemaphoreGetMutexHolder=1"
+	"compiler.cpp.extra_flags=-DINCLUDE_xSemaphoreGetMutexHolder=1"
+)
+
+target_use_arduino_libraries("STM32FreeRTOS")
+
+# include(fprime-arduino/cmake/toolchain/nucleo_H723ZG.cmake)
+# include(../../fprime-arduino/cmake/toolchain/nucleo_H723ZG.cmake)
